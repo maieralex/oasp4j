2a03d959d1e1f6d24e322bedc75ce2079f80651d
Pascal Dung <pascal.dung@th-koeln.de>
2015-10-27 16:06:05 +0100

    Move recipe table creation and dummydata insertion to V0004

---
+++ oasp4j-samples/oasp4j-sample-core/src/main/resources/db/migration/V0004__Add_recipe_table_and_data.sql
@@ -0,0 +1,16 @@
+CREATE CACHED TABLE PUBLIC.RECIPE(
+    id BIGINT NOT NULL,
+    modificationCounter INTEGER NOT NULL,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    price DECIMAL(19, 2),
+    imageId BIGINT
+);
+
+ALTER TABLE PUBLIC.RECIPE ADD CONSTRAINT PUBLIC.PK_RECIPE PRIMARY KEY(id);
+
+INSERT INTO RECIPE (id, modificationCounter, name, description, price) VALUES (0, 0, 'WienerSchnitzel', 'Schnitzel Wiener Art', 7.99);
+INSERT INTO RECIPE (id, modificationCounter, name, description, price) VALUES (1, 0, 'Spaghetti', 'Pasta nach Bolognese Art', 8.99);
+INSERT INTO RECIPE (id, modificationCounter, name, description, price) VALUES (2, 0, 'Kaiserschmarn', 'Leckere Delikatesse', 7.99);
+INSERT INTO RECIPE (id, modificationCounter, name, description, price) VALUES (3, 0, 'Sauerbraten', 'Sauerbraten nach rheinischer Art', 10.99);
+INSERT INTO RECIPE (id, modificationCounter, name, description, price, imageId) VALUES (4, 0, 'Asiapfanne', 'Frisches Gemuese aus dem Wok', 4.99, 10);
