a51f37df4c9a7add555641cfe4e66a962797cb7c
Pascal Dung <pascal.dung@th-koeln.de>
2016-01-13 12:22:38 +0100

    Add Use Case for Price Calculation. Add Rest Service for Price Calculation.

---
+++ oasp4j-samples/oasp4j-sample-core/src/main/java/io/oasp/gastronomy/restaurant/recipemanagement/logic/impl/usecase/UcCalculatePriceImpl.java
@@ -0,0 +1,38 @@
+package io.oasp.gastronomy.restaurant.recipemanagement.logic.impl.usecase;
+
+import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
+import io.oasp.gastronomy.restaurant.general.logic.api.UseCase;
+import io.oasp.gastronomy.restaurant.recipemanagement.logic.api.to.RecipeIngredientEto;
+import io.oasp.gastronomy.restaurant.recipemanagement.logic.api.usecase.UcCalculatePrice;
+import io.oasp.gastronomy.restaurant.recipemanagement.logic.base.usecase.AbstractRecipeUc;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Named;
+import java.util.Set;
+
+/**
+ * Created by pascaldung on 13.01.16.
+ */
+@Named
+@UseCase
+public class UcCalculatePriceImpl extends AbstractRecipeUc implements UcCalculatePrice {
+
+  @Override
+  @PermitAll
+  public Money getSummedPrice(Set<RecipeIngredientEto> ingredients) {
+
+    Double sum = 0.0;
+    Double FIXPRICE = 1.0;
+
+    if (ingredients == null) {
+      return new Money(sum);
+    }
+
+    for (RecipeIngredientEto ingredientEto :
+      ingredients) {
+      sum += ingredientEto.getAmount() * FIXPRICE;
+    }
+
+    return new Money(sum);
+  }
+}
